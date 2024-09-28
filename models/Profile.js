49ecfcd5a549644215e3e0a7b1a0a3d4ff8303e4
Izanalgit <izanalvarez@gmail.com>
2024-09-28 22:05:00 +0200

    feat : profile model

---
+++ models/Profile.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const profileSchema = new mongoose.Schema({
+    userId : {
+        type:mongoose.Schema.ObjectId,
+        require: true,
+        unique:true
+    },
+    bio : {
+        type:String,
+    },
+    profilePicture : {
+        type:String,
+    },
+    coverPhoto : {
+        type:String,
+    },
+    
+},{timestamps:true})
+
+const Profile = mongoose.model('profile',profileSchema);
+
+module.exports = Profile;
