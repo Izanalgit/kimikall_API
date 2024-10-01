9a9ab4bfb5d847328fbcd18537161e6260a1439a
Izanalgit <izanalvarez@gmail.com>
2024-10-01 13:40:11 +0200

    fix : cloudinary logics minor fixes

--- services/imagesServices.js
+++ services/imagesServices.js
@@ -1,4 +1,4 @@
-const cloudinary = require('../config/cloudinary');
+const cloudinary = require('../config/cloudinaryConfig');
 
 //Upload image to Cloudinary server
 const uploadImage = async (filePath, folder) => {
