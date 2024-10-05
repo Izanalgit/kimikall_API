26deeb4ffa795165f93bc47754b60f56c586624b
Izanalgit <izanalvarez@gmail.com>
2024-10-05 19:05:24 +0200

    feat : search profiles controller

--- controllers/contacts/searchContacts.js
+++ controllers/contacts/searchContacts.js
@@ -4,14 +4,29 @@ const {msgErr} = require('../../utils/errorsMessages');
 module.exports = async (req,res) => {
     
     const userId = req.user;
+    const payload = req.body.payload;
     
-    try {    
+    try {
 
-        const contactsList = await searchProfiles(userId,normalSearch,expandedSearch);
+        //No payload
+        if(!payload)
+            return res
+                .status(400)
+                .json({messageErr:msgErr.errPayloadRequired});
+
+        const {normalSearch,expandedSearch} = payload;
+
+        //Incorrect payload
+        if(!normalSearch)
+            return res
+                .status(400)
+                .json({messageErr:msgErr.errPayloadIncorrect});    
+
+        const profiles = await searchProfiles(userId,normalSearch,expandedSearch);
   
         return res
             .status(200)
-            .json({contactsList});
+            .json({profiles});
     
     }catch (err) {
         msgErr.errConsole(userId,'SEARCH PROFILES CONTACTS', err);
