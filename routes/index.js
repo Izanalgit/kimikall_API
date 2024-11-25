73854a8a36fd65306f0ed7516fc0a0dca179e21d
Izanalgit <izanalvarez@gmail.com>
2024-11-25 16:39:53 +0100

    feat : premy routes on index routes

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,6 @@ router.use('/profile', require('./profileRoutes'));
 router.use('/contacts', require('./contactsRoutes'));
 router.use('/chat', require('./messageRoutes'));
 router.use('/privacy', require('./privacyRoutes'));
+router.use('/premy', require('./premyRoutes'));
 
 module.exports = router;
