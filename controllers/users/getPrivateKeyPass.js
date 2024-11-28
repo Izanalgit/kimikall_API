9e941d6bcb87d6d12456f68bb0fe50a43ecbbcd9
Izanalgit <izanalvarez@gmail.com>
2024-11-28 18:19:54 +0100

    feat : new send key pass enroute

---
+++ controllers/users/getPrivateKeyPass.js
@@ -0,0 +1,29 @@
+const {sendPrivareKeyPass} = require('../../services/pairKeyServices');
+const {msgErr} = require('../../utils/errorsMessages');
+
+module.exports = async (req,res) => {
+    
+    const userId = req.user;
+    
+    try{
+
+        //Get profile user
+        const keys = await sendPrivareKeyPass(userId);   
+
+        const soloElPuebloSalvaAlPueblo={
+            rps:keys.reEncryptedPrivateKeyPassword,
+            riv:keys.reIv,
+            rsa:keys.reSalt
+        }
+
+        return res
+            .status(200)
+            .json({ soloElPuebloSalvaAlPueblo });
+
+    } catch (err) {
+        msgErr.errConsole(userId,'GET PRIVATE KEY PASS', err);
+        return res
+            .status(500)
+            .json({messageErr:msgErr.errApiInternal});
+    }
+};
