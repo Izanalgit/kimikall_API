d7d1c17831bd1256cd315e9fdcd3dc28c248a18c
Izanalgit <izanalvarez@gmail.com>
2024-11-29 19:23:09 +0100

    update : hex to hex encryption on messageCrypto util

--- utils/messageCrypto.js
+++ utils/messageCrypto.js
@@ -8,8 +8,10 @@ function encryptText(data){
     const iv = crypto.randomBytes(16);
 
     const cipher = crypto.createCipheriv(algorithm,key,iv);
-    
-    let encrypted = cipher.update(data,'utf8','hex');
+
+    const messageUtf8 = Buffer.from(data, 'base64').toString('hex');
+
+    let encrypted = cipher.update(messageUtf8,'hex','hex');
     encrypted += cipher.final('hex');
 
     return {encryptedData: encrypted, iv: iv.toString('hex')};
@@ -20,10 +22,12 @@ function decryptText(encryptedData, ivHex){
 
     const decipher = crypto.createDecipheriv(algorithm, key, Buffer.from(ivHex, 'hex'));
 
-    let decrypted = decipher.update(encryptedData, 'hex', 'utf8');
-    decrypted += decipher.final('utf8');
+    let decrypted = decipher.update(encryptedData, 'hex', 'hex');
+    decrypted += decipher.final('hex'); 
 
-    return decrypted;
+    const messageBase64 = Buffer.from(decrypted, 'hex').toString('base64');
+
+    return messageBase64;
 }
 
 module.exports = {encryptText, decryptText};
