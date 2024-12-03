8ac03775863273daf96ad47d7d17f34b1180cc86
Izanalgit <izanalvarez@gmail.com>
2024-12-03 11:21:35 +0100

    update : recover acount on sendgrid config

--- config/sendgridConfig.js
+++ config/sendgridConfig.js
@@ -9,7 +9,14 @@ const verificationEmail = {
     html:(key)=>`<p>Aquí tienes tu enlace de verificación: <a href="${process.env.HOST}/api/user/verify/${key}">Verificar cuenta</a></p>`
 }
 
+const forgottenPassEmail = {
+    subject:'Recupera tu cuenta',
+    text:(key)=>`Aquí tienes tu llave de recuperación: ${key}`,
+    html:(key)=>`<p>Aquí tienes tu llave de recuperación:</p> <h1><b>${key}</b></h1>`
+}
+
 module.exports = {
     sendgridConfig,
-    verificationEmail
+    verificationEmail,
+    forgottenPassEmail
 };
