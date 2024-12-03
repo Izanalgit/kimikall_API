4df7033987a987017a68f4d986430f2de7bf0d81
Izanalgit <izanalvarez@gmail.com>
2024-12-03 19:26:02 +0100

    update : added new logics and decrypt message to validations

--- validations/messageValidations.js
+++ validations/messageValidations.js
@@ -1,6 +1,8 @@
 const { body } = require('express-validator');
 const mongoose = require('mongoose');
 
+const forbiddenWords = ['instagram', 'wass', 'watsup', 'elle'];
+
 const messageValidation = [
 	body('payload.recep')
         .trim()
@@ -8,11 +10,29 @@ const messageValidation = [
         .withMessage('Se requiere de un usuario receptor para el mensaje')
         .custom(id => mongoose.isValidObjectId(id))
         .withMessage('El usuario receptor debe ser un id válido'),
-    // body('payload.message')
-    //     .trim()
-    //     .notEmpty()
-    //     .matches(/^[a-zA-Z0-9\s.,'-]*$/)
-    //     .withMessage('Se requiere de un cuerpo de mensaje')
+    body('messageValidation')
+        .trim()
+        .notEmpty()
+        .matches(/^[a-zA-Z0-9\s.,'-]*$/)
+        .withMessage('Se requiere de un cuerpo de mensaje')
+        // Phone number filter
+        .custom((value) => {
+            const phoneRegex = /(\+?\d{1,3}[-.\s]?)?(\(?\d{1,4}\)?[-.\s]?)?[\d-\s]{5,}/g;
+            if (phoneRegex.test(value)) {
+                throw new Error('No se permiten números de teléfono.');
+            }
+            return true;
+        })
+        // Words filter
+        .custom((value) => {
+            const lowerCaseMessage = value.toLowerCase(); 
+            for (let word of forbiddenWords) {
+                if (lowerCaseMessage.includes(word)) {
+                    throw new Error(`El mensaje contiene una palabra prohibida: ${word}`);
+                }
+            }
+            return true; 
+        })
 ]
 
 module.exports = { messageValidation }
