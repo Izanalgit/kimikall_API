7f914ee819132b177c2ca5a4d8fb9fd5494da7ce
Izanalgit <izanalvarez@gmail.com>
2024-12-10 18:39:57 +0100

    fix : check if allready user on singup controller

--- controllers/users/singUpUser.js
+++ controllers/users/singUpUser.js
@@ -1,34 +1,42 @@
 const {dbCreatePreUser, dbDeletePreUser} = require('../../services/preUserServices');
 const {sendVerificationEmail} = require('../../services/mailSenderServices');
 const {passHasher} = require('../../utils/passwordHasher');
+const {dbFindUser} = require('../../services/userServices')
 const {msgErr} = require('../../utils/errorsMessages');
 
 module.exports =async (req,res)=>{
     
-    const payload = req.body.payload;
+    const payload = req.body.payload
+
+    //Logued check
+    const sessionToken = req.user; 
+    if(sessionToken)
+        return res
+            .status(409)
+            .json({messageErr:msgErr.errSession(true)})
+
+    //No payload check
+    if(!payload)
+        return res
+            .status(400)
+            .json({messageErr:msgErr.errPayloadRequired});
+
+    const {name,email,pswd} = payload;
+
+    //Incorrect payload check
+    if(!name || !email || !pswd)
+        return res
+            .status(400)
+            .json({messageErr:msgErr.errPayloadIncorrect});
+
+    //Allready user check
+    const allreadyUser = await dbFindUser(email);
+    if(allreadyUser)
+        return res
+            .status(400)
+            .json({messageErr:msgErr.errGeneral('User allready created')});
 
     try{       
-
-        //Logued check
-        const sessionToken = req.user; 
-        if(sessionToken)
-            return res
-                .status(409)
-                .json({messageErr:msgErr.errSession(true)})
-
-        //No payload check
-        if(!payload)
-            return res
-                .status(400)
-                .json({messageErr:msgErr.errPayloadRequired});
-
-        const {name,email,pswd} = payload;
-
-        //Incorrect payload check
-        if(!name || !email || !pswd)
-            return res
-                .status(400)
-                .json({messageErr:msgErr.errPayloadIncorrect});
         
         //Create pre user
         const hashedPaswd = await passHasher(pswd); //hash user password
